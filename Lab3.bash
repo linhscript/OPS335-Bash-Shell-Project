8a805d93a821ceaafecc6e7676fbf9f23cb6f75f
Linh Ha Van <havanlinh304@gmail.com>
2018-11-08 09:20:48 -0500

    Update Lab3.bash

---
+++ Lab3.bash
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+##### Lab 3 ###########
+function check() {
+	if eval $1
+	then
+		echo -e "\e[32mOKAY_Babe. Good job \e[0m"
+	else
+		echo
+     		echo
+     		echo -e "\e[0;31mWARNING\e[m"
+     		echo
+     		echo
+     		echo $2
+     		echo
+     		exit 1
+	fi	
+}
+read -p "What is your Seneca username: " $username
+read -p "What is your IP Address of VM1" $IP
+$digit=$( echo "$IP" | awk -F. '{print $3}' )
+
+##Checking running script by root###
+if [ `id -u` -ne 0 ]
+then
+	echo "Must run this script by root" >&2
+	exit 1 
+fi
+
+####
+
+## Installing BIND Package ######
+echo 
+echo "############ Installing DNS ###########"
+echo 
+check "yum install bind*" ""
+cat > /etc/named.conf << EOF
+options {
+        directory "/var/named/";
+        allow-query {127.0.0.1; 192.168.$digit.0/24;};
+        forwarders { 192.168.40.2; };
+};
+zone "." IN {
+	type hint;
+        file "named.ca";
+};
+zone "localhost" {
+        type master;
+        file "named.localhost";
+};
+zone "$username.ops" {
+        type master;
+        file "mydb-for-$username-ops";
+};
+
+EOF
+
+
+
+
+
+
+
+
+
+
+
+
+
