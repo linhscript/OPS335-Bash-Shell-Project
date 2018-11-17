4b06cec744aa21da13330f964352f4bc6ce692be
Linh Ha Van <havanlinh304@gmail.com>
2018-11-17 02:40:51 -0500

    Update Assignment2.bash

--- Assignment2.bash
+++ Assignment2.bash
@@ -4,7 +4,7 @@
 function check() {
 	if eval $1
 	then
-		echo -e "\e[32mOK. Done \e[0m"
+		echo -e "\e[32mOK. GOOD \e[0m"
 	else
 		echo
      		echo
@@ -19,8 +19,8 @@ function check() {
 function require {
 	### ALL INPUT BEFORE CHECKING ####
 	
-	vms_name=(toronto ottawa cloyne)
-	vms_ip=(172.17.15.2 172.17.15.3 172.17.15.100)	
+	vms_name=(toronto ottawa kingston coburg milton)
+	vms_ip=(172.17.15.2 172.17.15.3 172.17.15.5 172.17.15.6 172.17.15.8)	
 	
 	#### Create Hash Table
 	
@@ -33,18 +33,33 @@ function require {
 	echo -e "\e[1;31m--------WARNING----------"
 	echo -e "\e[1mBackup your virtual machine to run this script \e[0m"
 	echo
-	read -p "Did you make a backup ? [Y/N]: " choice
+	read -p "Did you make a backup ?. If NO, It will do it for you. [Y/N]: " choice
 	while [[ "$choice" != "Y" && "$choice" != "Yes" && "$choice" != "y" && "$choice" != "yes" ]]
 	do
-		echo -e "\e[33mGo make a backup \e[0m" >&2
-		exit 1
+		echo -e "\e[33mBacking up in process \e[0m" >&2
+		for shut in $(virsh list --name)  ## --- shutdown vms to backup --- ###
+		do
+			virsh shutdown $shut
+			while virsh list | grep -iqs $shut
+			do
+				echo $shut is being shutdown. Wait
+                sleep 3
+			done
+		done
+		yum install pv -y > /dev/null
+		
+		for bk in $(ls /var/lib/libvirt/images/ | grep \.qcow2$)
+		do
+			echo "Backing up bk"
+			pv /var/lib/libvirt/images/$bk | gzip | pv  > /backup/full/$bk.backup.gz
+		done
 	done
 
 	### 2.Run script by Root
 	if [ `id -u` -ne 0 ]
 	then
 		echo "Must run this script by root" >&2
-		exit 1 
+		exit 2
 	fi
 
 	### 3.Checking VMs need to be online
@@ -55,15 +70,23 @@ function require {
 		if ! virsh list | grep -iqs $vm
 		then
 			echo -e "\e[1;31mMust turn on $vm  \e[0m" >&2
-			exit 2
+			exit 3
 		fi
 	done
-
-	### 4.Pinging check
-	# 4.1 Host check
+	
+	### 4.SSH and Pinging Check
+	check "ping -c 3 google.ca > /dev/null" "Host machine can not ping GOOGLE.CA, check INTERNET connection then run the script again"
 		
-
-
+	for ssh_vm in ${dict[@]} ## -- Checking VMS -- ##
+	do
+	check "ssh -o ConnectTimeout=5 $ssh_vm ls > /dev/null" "Can not SSH to ${!dict[$ssh_vm]}, check and run the script again "
+	check "ssh $ssh_vm ping -c 3 google.ca > /dev/null" "Can not ping GOOGLE.CA from ${!dict[$ssh_vm]}, check internet connection then run the script again"
+	done
+	
+	### 5.Checking jobs done from Assignment 1
+	
+	### 6.Checking jobs done from Assignment 1
+	
 
 
 
