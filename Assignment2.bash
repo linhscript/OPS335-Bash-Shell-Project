7582ff2da066798aeb0af1e4678b3a6ff4d68517
Van Linh Ha <vlha@myseneca.ca>
2018-11-19 15:51:03 -0500

    1

--- Assignment2.bash
+++ Assignment2.bash
@@ -87,6 +87,8 @@ function require {
 		fi
 	done
 
+
+
 	########################################
 	echo "Checking VMs status"
 	for vm in ${vms_name[@]}
@@ -411,3 +413,5 @@ ssh 172.17.15.2 "sed -i 's/.*MX.*/town.ontario.ops. IN MX 10 coburg.towns.ontari
 # fix selinux which prevents postfix
 #postfix_local_write_mail_spool --> on (coburg and kinsgton)
 
+#Check if it needs to clone any machine =>Yes=> turn on cloyne => ssh to cloyne => Comment Mac address > Turnoff cloyne => Clone machine => Turn on that machine with out turnning on cloyne
+#=> ssh to new machine with cloyne ip address => Also dumpxml to get infor => uncomment mac and replace with new mac => Change IP, hostname => restart machine 
