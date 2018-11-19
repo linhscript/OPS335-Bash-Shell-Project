2e3847ffe007c32169977f8062b271910707857c
Van Linh Ha <vlha@myseneca.ca>
2018-11-19 16:42:47 -0500

    1

--- Assignment2.bash
+++ Assignment2.bash
@@ -107,11 +107,11 @@ function require {
 
 	check "ping -c 3 google.ca > /dev/null" "Host machine can not ping GOOGLE.CA, check INTERNET connection then run the script again"
 		
-	for ssh_vm in ${dict[@]} ## -- Checking VMS -- ##
+	for ssh_vm in ${!dict[@]} ## -- Checking VMS -- ##
 	do
-	check "ssh -o ConnectTimeout=5 $ssh_vm ls > /dev/null" "Can not SSH to ${!dict[$ssh_vm]}, check and run the script again "
-	check "ssh $ssh_vm ping -c 3 google.ca > /dev/null" "Can not ping GOOGLE.CA from ${!dict[$ssh_vm]}, check internet connection then run the script again"
-	check "ssh $ssh_vm yum update -y" "Can not YUM UPDATE from ${!dict[$ssh_vm]}"
+	check "ssh -o ConnectTimeout=5 ${dict[$ssh_vm]} ls > /dev/null" "Can not SSH to $ssh_vm, check and run the script again "
+	check "ssh ${dict[$ssh_vm]} ping -c 3 google.ca > /dev/null" "Can not ping GOOGLE.CA from $ssh_vm, check internet connection then run the script again"
+	check "ssh ${!dict[$ssh_vm]} yum update -y" "Can not YUM UPDATE from $ssh_vm"
 	done
 	
 	### 5.Checking jobs done from Assignment 1 -------------------------
