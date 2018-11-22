cfffb067a49c2d9e33df23f791f5d5281e665267
Van Linh Ha <vlha@myseneca.ca>
2018-11-21 20:07:12 -0500

    12

--- Assignment2.bash
+++ Assignment2.bash
@@ -100,7 +100,7 @@ function require {
 				echo "Cloyne machine is starting"
 				sleep 3
 			done
-			sleep 3
+			sleep 5
 			check "ssh -o ConnectTimeout=8 172.17.15.100 ls > /dev/null" "Can not SSH to Cloyne, check and run the script again"
 			intcloyne=$( ssh 172.17.15.100 '( ip ad | grep -B 2 172.17.15 | head -1 | cut -d" " -f2 | cut -d: -f1 )' )  #### grab interface infor (some one has ens3)
 			maccloyne=$(ssh 172.17.15.100 grep "^HW.*" /etc/sysconfig/network-scripts/ifcfg-$intcloyne) #### grab mac address
@@ -149,6 +149,7 @@ function require {
 					echo "Cloyne machine is starting"
 					sleep 3
 				done
+				sleep 5
 				ssh 172.17.15.100 "sed -i 's/.*HW.*/${oldmac}/g' /etc/sysconfig/network-scripts/ifcfg-$intcloyne"
 				ssh 172.17.15.100 init 6
 		fi
