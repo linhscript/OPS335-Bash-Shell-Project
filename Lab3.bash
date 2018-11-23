c7ab1c590f3737e9f1e8add9ecba174ecf86abe0
Van Linh Ha <vlha@myseneca.ca>
2018-11-23 18:42:36 -0500

    12

--- Lab3.bash
+++ Lab3.bash
@@ -142,10 +142,17 @@ echo -e "###\e[32mConfiguration Done\e[m###"
 echo
 
 ### CONFIG USERNAME, HOSTNAME, DOMAIN VM1,2,3
-
-
-
-
+for (( i=2;i<=4;i++ ))
+do
+intvm=$( ssh 192.168.$digit.${i} '( ip ad | grep -B 2 192.168.$digit | head -1 | cut -d" " -f2 | cut -d: -f1 )' )
+ssh 192.168.$digit.${i} "echo vm${i-1}.$domain > /etc/hostname"
+check "ssh 192.168.$digit.${i} grep -v -e '^DNS.*' -e 'DOMAIN.*' /etc/sysconfig/network-scripts/ifcfg-$intvm > ipconf.txt" "File or directory not exist"
+echo "DNS1="192.168.$digit.1"" >> ipconf.txt
+echo "PEERDNS=no" >> ipconf.txt
+echo "DOMAIN=$domain" >> ipconf.txt
+check "scp ipconf.txt 192.168.$digit.${i}:/etc/sysconfig/network-scripts/ifcfg-$intvm > /dev/null" "Can not copy ipconf to VM${i}"
+rm -rf ipconf.txt > /dev/null
+done
 
 
 
