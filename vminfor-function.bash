e088a424b85f43d5b66eb8b9f906c7e3a36b2d4e
Linh Ha Van <havanlinh304@gmail.com>
2018-11-25 23:36:18 -0500

    Update vminfor-function.bash

--- vminfor-function.bash
+++ vminfor-function.bash
@@ -1,20 +1,27 @@
 function vminfo {
 
 ## Config DOMAIN, HOSTNAME, RESOLV File, Disable Network Manager
-	if [ -z "$2" ]
+## Need some arguments such as: IP HOSTNAME DNS1 DNS2 
+	if [ "$#" -lt 3 || "$#" -ge 5 ]
 	then
-	intvm=$( ssh 192.168.$digit. '( ip ad | grep -B 2 192.168.$digit | head -1 | cut -d" " -f2 | cut -d: -f1 )' )
-	ssh 192.168.$digit.${i} "echo vm$(($i-1)).$domain > /etc/hostname"
-	check "ssh 192.168.$digit.${i} grep -v -e '^DNS.*' -e 'DOMAIN.*' /etc/sysconfig/network-scripts/ifcfg-$intvm > ipconf.txt" "File or directory not exist"
-	echo "DNS1=$1" >> ipconf.txt
-	echo "PEERDNS=no" >> ipconf.txt
-	echo "DOMAIN=$domain" >> ipconf.txt
-	check "scp ipconf.txt 192.168.$digit.${i}:/etc/sysconfig/network-scripts/ifcfg-$intvm > /dev/null" "Can not copy ipconf to VM${i}"
-	rm -rf ipconf.txt > /dev/null
-	ssh 192.168.$digit.${i} "echo "search $domain" > /etc/resolv.conf"
-	ssh 192.168.$digit.${i} "echo "nameserver 192.168.${digit}.1" >> /etc/resolv.conf"
-	ssh 192.168.$digit.${i} "systemctl stop NetworkManager"
-	ssh 192.168.$digit.${i} "systemctl disable NetworkManager"
+		echo -e "\e[31mMissing or Unneeded arguments\e[m"
+		exit 2
+	else
+		intvm=$( ssh $1 '( ip ad | grep -B 2 192.168.$digit | head -1 | cut -d" " -f2 | cut -d: -f1 )' )
+		ssh $1 "echo $2 > /etc/hostname"
+		check "ssh $1 grep -v -e '^DNS.*' -e 'DOMAIN.*' /etc/sysconfig/network-scripts/ifcfg-$intvm > ipconf.txt" "File or directory not exist"
+		echo "PEERDNS=no" >> ipconf.txt
+		echo "DNS1=$3" >> ipconf.txt
+		if [ ! -z "$4" ]
+		then
+			echo "DNS2=$4" >> ipconf.txt
+		fi
+		echo "DOMAIN=$domain" >> ipconf.txt
+		check "scp ipconf.txt $1:/etc/sysconfig/network-scripts/ifcfg-$intvm > /dev/null" "Can not copy ipconf to VM $2"
+		ssh $1 "echo "search $domain" > /etc/resolv.conf"
+		ssh $1 "systemctl stop NetworkManager"
+		ssh $1 "systemctl disable NetworkManager"
+		rm -rf ipconf.txt > /dev/null
 	fi
 }
 #
