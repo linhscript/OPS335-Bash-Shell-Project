215d7886b6e96c2fbddf3560d6329cfbb12b132c
Linh Ha Van <havanlinh304@gmail.com>
2018-11-25 19:53:10 -0500

    Update vminfor-function.bash

--- vminfor-function.bash
+++ vminfor-function.bash
@@ -1,10 +1,11 @@
 function vminfo {
 
 ## Config DOMAIN, HOSTNAME, RESOLV File, Disable Network Manager
-	intvm=$( ssh 192.168.$digit.${i} '( ip ad | grep -B 2 192.168.$digit | head -1 | cut -d" " -f2 | cut -d: -f1 )' )
+	if [ -z "$2" ]
+	intvm=$( ssh 192.168.$digit. '( ip ad | grep -B 2 192.168.$digit | head -1 | cut -d" " -f2 | cut -d: -f1 )' )
 	ssh 192.168.$digit.${i} "echo vm$(($i-1)).$domain > /etc/hostname"
 	check "ssh 192.168.$digit.${i} grep -v -e '^DNS.*' -e 'DOMAIN.*' /etc/sysconfig/network-scripts/ifcfg-$intvm > ipconf.txt" "File or directory not exist"
-	echo "DNS1="192.168.$digit.1"" >> ipconf.txt
+	echo "DNS1=$1" >> ipconf.txt
 	echo "PEERDNS=no" >> ipconf.txt
 	echo "DOMAIN=$domain" >> ipconf.txt
 	check "scp ipconf.txt 192.168.$digit.${i}:/etc/sysconfig/network-scripts/ifcfg-$intvm > /dev/null" "Can not copy ipconf to VM${i}"
