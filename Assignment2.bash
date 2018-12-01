22f6186dc37a810055da0c273e147f0919810a08
flashsixnine69 <vlha@myseneca.ca>
2018-11-30 22:43:46 -0500

    12

--- Assignment2.bash
+++ Assignment2.bash
@@ -12,7 +12,7 @@ function require {
 	     		echo -e "\e[0;31mWARNING\e[m"
 	     		echo
 	     		echo
-	     		echo $2
+	     		zenity --error --title="An Error Occurred" --text=$2
 	     		echo
 	     		exit 1
 		fi	
