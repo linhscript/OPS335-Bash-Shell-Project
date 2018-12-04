be11f43d866318cd56c95813ea4a86e52a06cdb0
FLASHSIX69 <havanlinh304@gmail.com>
2018-12-03 23:01:05 -0500

    Update Assignment2.bash

--- Assignment2.bash
+++ Assignment2.bash
@@ -511,7 +511,7 @@ cat > /root/Assignment2-information.txt << EOF
 
 + Users for Samba: $username-1 $username-2 $username-admin
 + Password to login: $password
-+ Path: 172.17.15.10/documents
++ Path: 172.17.15.8/documents
 
 ## All the above information will be stored in  /root/Assignment2-information.txt ##
 ------------------------------------------------------------
