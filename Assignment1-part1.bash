c4b2ba348a14c282358d33a0c8cee5806d17fa9a
Linh Ha Van <havanlinh304@gmail.com>
2018-12-06 12:25:08 -0500

    123

---
+++ Assignment1-part1.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create Network 335assign
+# Generate SSH key
+
+# Configuration
+# PermitRootLogin Status:
+
+# Firewalld Status: Disable
+# Shell Script contents (/root/bin/assnBackup.bash
+# Full Backup Status
+#Crontab Log 
+#Incremental Backup 
