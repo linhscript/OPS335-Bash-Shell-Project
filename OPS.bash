db3ab0db5e167bc76f6d4055df99dd4f34a787d3
Linh Ha Van <havanlinh304@gmail.com>
2019-01-06 20:17:57 -0500

    Create OPS.bash

---
+++ OPS.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+zenity --list --title="List of works" --checklist\
+ --column="Options" --column="Details" \
+ 1 Lab_1\
+ 2 Lab_2a\
+ 3 Lab_2b\
+ 4 Lab_3\
+ 5 Lab_4a\
+ 6 Lab_4b\
+ 7 Lab_5\
+ 8 Lab_6\
+ 9 Lab_7\
+ 10 Lab_8\
+ 11 Assignment1_Part_1\
+ 12 Assignment1_Part_2\
+ 13 Assignment2\
